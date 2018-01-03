8c6cc3d7ad9665d642d2ac83938e7fa1d18083cb
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2018-01-03 15:38:07 +0100

    Init file with font settings and modeline hiding during startup..

---
+++ init.el
@@ -0,0 +1,13 @@
+;;; private/boy/init.el -*- lexical-binding: t; -*-
+
+;; Prevents the unstyled mode-line flash at startup
+(setq-default mode-line-format nil)
+
+(setq
+      doom-font (font-spec :family "Fira Mono" :size 12)
+      doom-variable-pitch-font (font-spec :family "Fira Sans")
+      doom-unicode-font (font-spec :family "DejaVu Sans Mono")
+      doom-big-font (font-spec :family "Fira Mono" :size 19)
+
+      org-ellipsis " ▼ ")
+
