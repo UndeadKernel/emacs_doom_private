adb5719f1d7a95eafb82d506cd490a350a935758
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2018-03-09 11:44:17 +0100

    Larger font size.

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
-;;; private/boy/init.el -*- lexical-binding: t; -*-
+;;; config/boy/init.el -*- lexical-binding: t; -*-
 
 ;; Prevents the unstyled mode-line flash at startup
 (setq-default mode-line-format nil)
 
 (setq
-      doom-font (font-spec :family "Fira Mono" :size 12)
+      doom-font (font-spec :family "Fira Mono" :size 14)
       doom-variable-pitch-font (font-spec :family "Fira Sans")
       doom-unicode-font (font-spec :family "DejaVu Sans Mono")
       doom-big-font (font-spec :family "Fira Mono" :size 19)
