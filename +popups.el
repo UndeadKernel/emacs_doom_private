ed38d7a15a54677d62fd0f89668962a7f1c49d70
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2018-03-21 16:53:38 +0100

    Adapt magit popup rules to latest magit version.

--- +popups.el
+++ +popups.el
@@ -2,22 +2,20 @@
 
 ;; Popup settings
 
-(remove-hook '+popup-display-buffer-actions 'display-buffer-in-side-window)
-(add-hook '+popup-display-buffer-actions #'+popup-display-buffer t)
+;; (remove-hook '+popup-display-buffer-actions 'display-buffer-in-side-window)
+;; (add-hook '+popup-display-buffer-actions #'+popup-display-buffer t)
+
+(set! :popup "^\\*Ibuffer\\*" nil '((modeline . popup) (select . t)))
 
 (after! magit
   ;(set! :popup "^\\*magit:*" '((slot . -1) (side . right) (size . 60)) '((modeline . nil) (select)))
-  (set! :popup "^\\*Magit" '((slot . -1) (side . right) (size . 60)) '((modeline . nil) (select)))
+  (set! :popup "^magit:" '((slot . -1) (side . right) (size . 60)) '((modeline . nil) (select)))
   (set! :popup "^\\*magit.*popup\\*" '((slot . 1) (side . right) (size . 50)) '((modeline . nil) (select . t)))
   (set! :popup "^\\*magit-revision:.*" '((slot . 0) (side . right) (window-height . 0.6)) '((modeline . nil) (select . t)))
   (set! :popup "^\\*magit-diff:.*" '((slot . 0) (side . right) (window-height . 0.6)) '((modeline . nil) (select . nil)))
-  (set! :popup "^\\*magit-log:.*" '((vslot . 1) (side . right) (size . 130)) nil))
+  (set! :popup "^magit-log:.*" '((vslot . 1) (side . right) (size . 130)) nil))
 
 (after! ein
   (set! :popup "^\\*ein:notebooklist .*" '((side . left) (size . 60)) '((select . t)))
   (set! :popup "^\\*ein: .*" nil '(modeline . t)))
-;(set! :popup "^\\*magit" :ignore)
-;(set! :popup "^\\*magit:*" '((side . right) (slot . 0)) '((modeline . t)))
-;; (set! :popup "^\\*magit-log:*" '((side . right) (slot . +1)))
 
-(set! :popup "^\\*Ibuffer\\*" nil '((modeline . popup) (select . t)))
