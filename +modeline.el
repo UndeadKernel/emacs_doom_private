351130dd642e18a6e084fc99e70c7d29ad3a3a5e
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2018-03-21 16:52:57 +0100

    Comment out modeline modifications, latest DOOM shows borders adequately.

--- +modeline.el
+++ +modeline.el
@@ -2,19 +2,19 @@
 
 ;; Modeline settings
 
-(def-modeline-segment! tiny-bar
-  "The bar regulates the height of the mode-line in GUI Emacs.
-Returns \"\" to not break --no-window-system."
-  (if (display-graphic-p)
-      (+doom-modeline--make-xpm
-       (face-background (if (active)
-                            'doom-modeline-bar
-                          'doom-modeline-inactive-bar)
-                        nil t)
-       14
-       +doom-modeline-bar-width)
-    ""))
+;; (def-modeline-segment! tiny-bar
+;;   "The bar regulates the height of the mode-line in GUI Emacs.
+;; Returns \"\" to not break --no-window-system."
+;;   (if (display-graphic-p)
+;;       (+doom-modeline--make-xpm
+;;        (face-background (if (active)
+;;                             'doom-modeline-bar
+;;                           'doom-modeline-inactive-bar)
+;;                         nil t)
+;;        14
+;;        +doom-modeline-bar-width)
+;;     ""))
 
-; Set the modeline of popups as an empty dark line with a tiny blue strip.
-(def-modeline! popup (tiny-bar ) nil)
-(map-put +popup-default-parameters 'modeline 'popup)
+;; ; Set the modeline of popups as an empty dark line with a tiny blue strip.
+;; (def-modeline! popup (tiny-bar ) nil)
+;; (map-put +popup-default-parameters 'modeline 'popup)
