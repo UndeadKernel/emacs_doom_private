546ccd2989eae2651017fa41e27c1ed9060ec1f5
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2018-04-13 14:51:53 +0200

    In LaTeX mode, use \(\) instead of $$
    Reorganize some commands

--- +latex.el
+++ +latex.el
@@ -8,9 +8,6 @@
             (lambda ()
               (setq TeX-quote-language `("ngerman" "``" "''" ,TeX-quote-after-quote)))))
 
-;; Set the fill column to something large so that we can fold without problems.
-(add-hook! LaTeX-mode (set-fill-column 99999))
-
   ;; Load the org table package
 (def-package! org-table ;; internal package
   :commands (orgtbl-mode)
@@ -23,21 +20,25 @@
           "C-c o s"  #'+boy/print-table-send-cmd
           "C-c o r"  #'+boy/print-table-rcv-cmd)))
 
-;; Change some fonts from the doom-one theme for the sections in LaTeX.
 (add-hook! LaTeX-mode
+  ;; Change some fonts from the doom-one theme for the sections in LaTeX
   (custom-theme-set-faces 'doom-one
   '(font-latex-sectioning-1-face ((t (:background "#23272e" :foreground "#a9a1e1" :weight bold
                                       :bold bold :height 1.6))))
   '(font-latex-sectioning-2-face ((t (:inherit 'font-latex-sectioning-1-face
-                                      :foreground "#51afef" :height 0.9))))
+                                               :foreground "#51afef" :height 0.9))))
   '(font-latex-sectioning-3-face ((t (:inherit 'font-latex-sectioning-2-face
-                                      :foreground "#a9a1e1" :height 0.9))))
+                                               :foreground "#a9a1e1" :height 0.9))))
   '(font-latex-sectioning-4-face ((t (:inherit 'font-latex-sectioning-3-face
-                                      :foreground "violet" :height 0.9))))
+                                               :foreground "violet" :height 0.9))))
   '(font-latex-sectioning-5-face ((t (:inherit 'font-latex-sectioning-4-face
-                                      :foreground "white" :height 1.0))))))
+                                               :foreground "white" :height 1.0)))))
+  ;; Set the fill column to something large so that we can fold without problems
+  (set-fill-column 99999)
+  ;; In LaTeX mode, insert \( \) instead of $$ when pressing "$"
+  (set (make-variable-buffer-local 'TeX-electric-math) (cons "\\(" "\\)")))
 
-;; Custom folding of some macros and commands I normally use.
+;; Custom folding of some macros and commands I normally use
 (after! tex-fold
   ;; Set custom folds for Macros
   (add-to-list 'TeX-fold-macro-spec-list '("{1}" ("ac" "acf" "title")))
