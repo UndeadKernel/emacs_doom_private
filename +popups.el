a50d2b63d59d1aab8752976f89a1fbc832677a9d
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2018-05-29 15:36:25 +0200

    Better magit popup rules

--- +popups.el
+++ +popups.el
@@ -1,4 +1,4 @@
-;;; ../../../doom_private/+popups.el -*- lexical-binding: t; -*-
+;;; +popups.el -*- lexical-binding: t; -*-
 
 ;; Popup settings
 
@@ -8,12 +8,26 @@
 (set! :popup "^\\*Ibuffer\\*" nil '((modeline . popup) (select . t)))
 
 (after! magit
-  ;(set! :popup "^\\*magit:*" '((slot . -1) (side . right) (size . 60)) '((modeline . nil) (select)))
-  (set! :popup "^magit:" '((slot . -1) (side . right) (size . 60)) '((modeline . nil) (select)))
-  (set! :popup "^\\*magit.*popup\\*" '((slot . 1) (side . right) (size . 50)) '((modeline . nil) (select . t)))
-  (set! :popup "^\\*magit-revision:.*" '((slot . 0) (side . right) (window-height . 0.6)) '((modeline . nil) (select . t)))
-  (set! :popup "^\\*magit-diff:.*" '((slot . 0) (side . right) (window-height . 0.6)) '((modeline . nil) (select . nil)))
-  (set! :popup "^magit-log:.*" '((vslot . 1) (side . right) (size . 130)) nil))
+  (set! :popup "^\\(?: ?\\*\\)?magit.*: "
+    '((slot . -1) (side . right) (size . 80))
+    '((select . t) (quit . nil)))
+  (set! :popup "^\\*magit.*popup\\*"
+    '((slot . 0) (side . right))
+    '((select . t)))
+  (set! :popup "^\\(?: ?\\*\\)?magit-revision:.*"
+    '((slot . 2) (side . right) (window-height . 0.6))
+    '((select . t)))
+  (set! :popup "^\\(?: ?\\*\\)?magit-diff:.*"
+    '((slot . 2) (side . right) (window-height . 0.6))
+    '((select . nil))))
+
+;; (after! magit
+;;   ;(set! :popup "^\\*magit:*" '((slot . -1) (side . right) (size . 60)) '((modeline . nil) (select)))
+;;   (set! :popup "^magit:" '((slot . -1) (side . right) (size . 60)) '((modeline . nil) (select)))
+;;   (set! :popup "^\\*magit.*popup\\*" '((slot . 1) (side . right) (size . 50)) '((modeline . nil) (select . t)))
+;;   (set! :popup "^\\*magit-revision:.*" '((slot . 0) (side . right) (window-height . 0.6)) '((modeline . nil) (select . t)))
+;;   (set! :popup "^\\*magit-diff:.*" '((slot . 0) (side . right) (window-height . 0.6)) '((modeline . nil) (select . nil)))
+;;   (set! :popup "^magit-log:.*" '((vslot . 1) (side . right) (size . 130)) nil))
 
 (after! ein
   (set! :popup "^\\*ein:notebooklist .*" '((side . left) (size . 60)) '((select . t)))
