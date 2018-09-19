1bbf1dee7b19f519372f651130b3d7bc7e4672d2
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2018-09-19 14:37:04 +0200

    popup: undo tree and org src

--- +popups.el
+++ +popups.el
@@ -7,6 +7,13 @@
   (set-popup-rule! "^\\*Ilist"
     :side 'right :size 35 :quit nil :select t :ttl 0))
 
+;; Larger undo tree window
+(set-popup-rule! " \\*undo-tree\\*" :slot 2 :side 'left :size 20 :modeline nil :select t :quit t)
+
+;; For better editting org src blocks
+(after! org
+  (set-popup-rule! "^\\*Org Src" :side 'bottom :slot -2 :height 0.6 :width 0.5 :select t :autosave t :ttl nil :quit nil :select t))
+
 ;; (set-popup-rule! "^\\*Ibuffer\\*"  :select t)
 
 ;; (after! magit
