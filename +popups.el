dd698b33ae550adc1ce5b214acf40f7257d9e6ce
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2018-10-15 10:34:48 +0200

    popups: corret rules, for real this time

--- +popups.el
+++ +popups.el
@@ -5,15 +5,15 @@
 ;; Select the IList buffer when it is shown
 (after! imenu-list
   (set-popup-rule! "^\\*Ilist"
-    :side right :size 35 :quit nil :select t :ttl 0))
+    :side 'right :size 35 :quit nil :select t :ttl 0))
 
 ;; Larger undo tree window
 (after! undo-tree
-  (set-popup-rule! " \\*undo-tree\\*" :slot 2 :side right :size 40 :modeline nil :select t :quit t))
+  (set-popup-rule! " \\*undo-tree\\*" :slot 2 :side 'right :size 40 :modeline nil :select t :quit t))
 
 ;; Larger org src edit
 (after! org
-  (set-popup-rule! "^\\*Org Src" :side bottom :slot -2 :height 0.6 :width 0.5 :select t :autosave t :ttl nil :quit nil))
+  (set-popup-rule! "^\\*Org Src" :side 'bottom :slot -2 :height 0.6 :width 0.5 :select t :autosave t :ttl nil :quit nil))
 
 ;; For better editting org src blocks
 (after! ob-ipython
