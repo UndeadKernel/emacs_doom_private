39dd28b3719580fd8772892092020aacd088fd9c
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2018-11-23 16:54:54 +0100

    misc: undo tree stuff and remove smartparen things

--- config.el
+++ config.el
@@ -44,20 +44,30 @@
 (after! undo-tree
   (setq undo-tree-auto-save-history nil))
 
-;; (after! undo-tree
-;;     (advice-remove #'undo-tree-make-history-save-file-name #'doom*undo-tree-make-history-save-file-name)
-;;     (advice-remove #'undo-list-transfer-to-tree #'doom*strip-text-properties-from-undo-history)
-;;     (advice-remove #'undo-tree-save-history #'doom*compress-undo-tree-history)
-;;     (advice-remove #'undo-tree-load-history #'doom*shut-up))
+(after! ibuffer
+  ;; nearly all of this is the default layout
+  (setq ibuffer-formats
+        '((mark modified read-only " "
+                (name 50 50 :left :elide) ; changed
+                " "
+                (size 9 -1 :right)
+                " "
+                (mode 16 16 :left :elide)
+                " " filename-and-process)
+          (mark " "
+                (name 16 -1)
+                " " filename))))
 
-;; (after! undo-tree
-;;   (advice-remove #'undo-tree-make-history-save-file-name #'doom*undo-tree-make-history-save-file-name))
 ;; Pop mark improvements (http://endlessparentheses.com/faster-pop-to-mark-command.html)
 ;; pop mark as in C-u C-SPC C-SPC C-SPC ...
 (setq set-mark-command-repeat-pop t)
 ;; Pop until marker actually moves
 (advice-add 'pop-to-mark-command :around #'+boy/multi-pop-to-mark)
 
+
+;; ---------------------------------------------------------------------------------
+;; Additional Packages
+;; ---------------------------------------------------------------------------------
 
 ;; Show me where I made the last change in a document.
 (def-package! goto-last-change
@@ -79,59 +89,3 @@
   :init
   (setq ag-highlight-search t
         ag-reuse-buffers t))
-
-(after! ibuffer
-  ;; nearly all of this is the default layout
-  (setq ibuffer-formats
-        '((mark modified read-only " "
-                (name 50 50 :left :elide) ; change: 30s were originally 18s
-                " "
-                (size 9 -1 :right)
-                " "
-                (mode 16 16 :left :elide)
-                " " filename-and-process)
-          (mark " "
-                (name 16 -1)
-                " " filename))))
-
-;; disable :unless predicates with (sp-pair "'" nil :unless nil)
-;; disable :post-handlers with (sp-pair "{" nil :post-handlers nil)
-;; ...or specific :post-handlers with (sp-pair "{" nil :post-handlers '(:rem ("| " "SPC")))
-
-;; (after! smartparens
-;;   ;; Autopair quotes more conservatively; if I'm next to a word/before another
-;;   ;; quote, I likely don't want another pair.
-;;   (let ((unless-list '(sp-point-before-word-p
-;;                        sp-point-after-word-p
-;;                        sp-point-before-same-p)))
-;;     (sp-pair "'"  nil :unless unless-list)
-;;     (sp-pair "\"" nil :unless unless-list))
-
-;;   ;; Expand {|} => { | }
-;;   ;; Expand {|} => {
-;;   ;;   |
-;;   ;; }
-;;   (dolist (brace '("(" "{" "["))
-;;     (sp-pair brace nil
-;;              :post-handlers '(("||\n[i]" "RET") ("| " "SPC"))
-;;              ;; I likely don't want a new pair if adjacent to a word or opening brace
-;;              :unless '(sp-point-before-word-p sp-point-before-same-p)))
-
-;;   ;; Don't do square-bracket space-expansion where it doesn't make sense to
-;;   (sp-local-pair '(emacs-lisp-mode org-mode markdown-mode gfm-mode)
-;;                  "[" nil :post-handlers '(:rem ("| " "SPC")))
-
-;;   ;; Highjacks backspace to:
-;;   ;;  a) balance spaces inside brackets/parentheses ( | ) -> (|)
-;;   ;;  b) delete space-indented `tab-width' steps at a time
-;;   ;;  c) close empty multiline brace blocks in one step:
-;;   ;;     {
-;;   ;;     |
-;;   ;;     }
-;;   ;;     becomes {|}
-;;   ;;  d) refresh smartparens' :post-handlers, so SPC and RET expansions work
-;;   ;;     even after a backspace.
-;;   ;;  e) properly delete smartparen pairs when they are encountered, without the
-;;   ;;     need for strict mode.
-;;   ;;  f) do none of this when inside a string
-;;   (advice-add #'delete-backward-char :override #'doom/delete-backward-char))
