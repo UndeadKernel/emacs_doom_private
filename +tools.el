59ada6390d93cd20e764f2e1d2d82e7ddafe8a4f
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2019-01-09 14:17:47 +0100

    ein: remove deprecated `set` function

--- +tools.el
+++ +tools.el
@@ -1,13 +1,10 @@
 ;;; +tools.el --- description -*- lexical-binding: t; -*-
 
 ;; Magit config
-(after! magit
-  ;; Show differences at the word level when a hunk is selected.
-  (setq magit-diff-refine-hunk t))
 (add-hook! magit-mode (visual-line-mode +1))
 
 ;; EIN config
-(set! :ein-notebook-dir "~/Documents/CASED/Development")
+(setq +ein-notebook-dir "~/Documents/CASED/Development")
 (add-hook! ein:notebook-multilang-mode
   (map! :map ein:notebook-mode-map
         "M-p" #'+boy/up-scroll
