fbf02fabf978eb6808da9241bb94c67afd13a1f3
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2019-01-16 16:38:26 +0100

    org: binding to display inline images

--- +bindings.el
+++ +bindings.el
@@ -85,7 +85,8 @@
    :desc "Export beamer as latex"  "e l B" #'org-beamer-export-as-latex
    :desc "Export beamer as pdf"    "e l P" #'org-beamer-export-to-pdf
    :desc "Link store"              "l"     #'org-store-link
-   :desc "Org hydra"               "h"     #'+boy/org-babel-hydra/body)
+   :desc "Org hydra"               "h"     #'+boy/org-babel-hydra/body
+   :desc "Display inline images"   "i"     #'org-display-inline-images)
  ;; Snippets
  (:prefix "C-c s"
    :desc "New snippet"           "n" #'yas-new-snippet
