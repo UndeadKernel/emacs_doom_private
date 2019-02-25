2e3217202c72de5d9a0cb4c5bc1deb0717299384
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2019-02-25 13:37:55 +0100

    theme: consistently change frame border, org block

--- +themes.el
+++ +themes.el
@@ -16,15 +16,21 @@
   (setq preview-scale 1.2)
   (set-face-attribute 'preview-reference-face nil :background (doom-color 'bg)))
 
-;(add-hook! 'doom-load-theme-hook)
-;; (after! font-latex
-;;   (doom-themes-set-faces nil
-;;     '(font-latex-sectioning-1-face :height 1.6)))
-;; (after! font-latex
-;;   (custom-theme-set-faces 'user
-;;     '(font-latex-sectioning-2-face ((t (:height 1.6))))))
+;; Color the border of windows according to the `highlight' color of the doom's theme
+(add-hook! 'doom-load-theme-hook
+  (set-face-attribute 'vertical-border nil :foreground (doom-color 'highlight))
+  (after! org
+    (when (custom-theme-enabled-p 'doom-one)
+      ;; Purple boxes for Org BEGIN_SRC and END_SRC
+      (set-face-attribute 'org-block-begin-line nil
+                          :background "#5c3d5c"
+                          ;:foreground "#744d74"
+                          ;:foreground "#5c3d5c"
+                          :foreground "#a16ba1"
+                          :weight 'bold
+                          :height 0.9
+                          :box '(:line-width 2 :color "#5c3d5c")))))
 
-;; Purple boxes for Org BEGIN_SRC and END_SRC
 (add-hook! org-mode
   ;; Document title font
   (set-face-attribute 'org-document-title nil :height 2.0)
@@ -35,25 +41,4 @@
   ;; Face of ellipsis symbol (Purple like keywords)
   (set-face-attribute 'org-ellipsis nil :foreground "#c678dd")
   ;; Face of the entire headline of a DONE line
-  (set-face-attribute 'org-headline-done nil :foreground nil)
-  (cond 
-   ;; If doom-one theme is enabled
-   ((custom-theme-enabled-p 'doom-one)
-      ;; Change the style of the BEGIN_SRC and RESULT blocks
-      (set-face-attribute 'org-block-begin-line nil
-                          :background "#5c3d5c"
-                          ;:foreground "#744d74"
-                          ;:foreground "#5c3d5c"
-                          :foreground "#a16ba1"
-                          :weight 'bold
-                          :height 0.9
-                          :box '(:line-width 2 :color "#5c3d5c")))
-   ;; For other themes, disable the changes
-   (t
-      (set-face-attribute 'org-block-begin-line nil
-                          :background unspecified
-                          :foreground unspecified
-                          :weight unspecified
-                          :height unspecified
-                          :box unspecified
-                          :inherit org-meta-line))))
+  (set-face-attribute 'org-headline-done nil :foreground nil))
