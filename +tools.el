a6c15f0714da9f381e8f77de41d3e2b7c7806ad8
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2019-03-05 17:20:13 +0100

    tools: annotation buffers enable visual-line-mode

--- +tools.el
+++ +tools.el
@@ -1,5 +1,17 @@
 ;;; +tools.el --- description -*- lexical-binding: t; -*-
 
+;; pdf-tools config
+;; enable visual-line-mode in the buffer where annotations
+;; ... are shown (*Contents*)
+(after! pdf-tools
+  (defun boy/annot-visual-line (_id _buffer)
+    (let ((contents-buf (get-buffer "*Contents*")))
+      (when (and contents-buf (not visual-line-mode))
+        (with-current-buffer contents-buf
+          (visual-line-mode)))))
+  ;; advice the function responsible for creating the *Contents* buffer
+  (advice-add 'pdf-annot-list-context-function :after #'boy/annot-visual-line))
+
 ;; Magit config
 (add-hook! magit-mode (visual-line-mode +1))
 
