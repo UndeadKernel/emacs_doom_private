fff8f61a820a830921a3774c9faa10c4b5db19c3
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2019-03-11 15:36:27 +0100

    ui: switch-window changes to real buffers
    Or popups, depending on the function called.

--- +ui.el
+++ +ui.el
@@ -10,15 +10,58 @@
 ;; Reuse dired buffers
 (put 'dired-find-alternate-file 'disabled nil)
 
+;; Use switch-window more intelligently
+;; ... either switch only real buffers or popup buffers
+(when (and (featurep! :ui popup) (featurep! :ui window-select +switch-window))
+  (defvar +boy--switch-popups nil "Is t when we are switching between popups.")
 
-;; ;; From bitwalker's private config
-;; (after! neotree
-;;   ;; When switching to a file in the current project, expand the directory
-;;   ;; tree to the new file buffer
-;;   (add-hook! 'find-file-hook
-;;     (if (and (buffer-file-name) (neo-global--window-exists-p))
-;;         ;; And only if the file is a child of the current neotree root
-;;         (if (neo-global--file-in-root-p (buffer-file-name))
-;;             ;; We need to trigger neotree-find then switch back to the buffer we just opened
-;;             (save-current-buffer (neotree-find))))))
+  (defun +boy--window-list (frame minibuf window)
+    "Return a list of windows depending on the variable +BOY-SWITCH-POPUPS.
 
+When +BOY-SWITCH-POPUPS is t, only return popup windows"
+    (let ((windows (window-list frame minibuf window)))
+      (if (bound-and-true-p +boy--switch-popups)
+          (seq-filter #'+popup-window-p windows)
+        (seq-remove #'+popup-window-p windows))))
+
+  (defun +boy/switch-window (&optional popups)
+    "Trigger switching between popups or normal windows using `switch-window' and
+our patches."
+    (interactive "P")
+    (if popups
+        (let ((+boy--switch-popups t))
+          (switch-window))
+      (switch-window)))
+
+  ;; patched functions of the `switch-window' package
+
+  (el-patch-feature switch-window)
+  (after! switch-window
+    (el-patch-defun switch-window--list (&optional from-current-window)
+      "List windows for current frame.
+It will start at top left unless FROM-CURRENT-WINDOW is not nil"
+      (let ((relative (or from-current-window
+                          switch-window-relative))
+            (frames (if (bound-and-true-p switch-window-multiple-frames)
+                        (funcall switch-window-frame-list-function)
+                      (list (selected-frame)))))
+        (cl-loop for frm in (if relative
+                                (cons (selected-frame)
+                                      (cl-remove (selected-frame) frames))
+                              (cl-sort frames
+                                       'switch-window--compare-frame-positions))
+                 append ((el-patch-swap window-list +boy--window-list)
+                         frm nil
+                         (unless (and relative
+                                      (equal frm (selected-frame)))
+                           (frame-first-window frm))))))
+
+    (el-patch-defun switch-window--other-window-or-frame ()
+      "If `switch-window-multiple-frames' is set cycle through all visible
+windows from all frames. Call `other-window' otherwise."
+      (if switch-window-multiple-frames
+          (switch-window--select-window (next-window nil nil 'visible))
+        (el-patch-swap (other-window 1)
+                       (if (bound-and-true-p +boy--switch-popups)
+                           (+popup/other)
+                         (other-window 1)))))))
