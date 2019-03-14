c9a9aa47dcefa52ddb125d8229824f44ba50922e
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2019-03-14 15:10:16 +0100

    pdf: customizations

--- +tools.el
+++ +tools.el
@@ -11,6 +11,23 @@
           (visual-line-mode)))))
   ;; advice the function responsible for creating the *Contents* buffer
   (advice-add 'pdf-annot-list-context-function :after #'boy/annot-visual-line))
+;; automatically activate new annotations
+(setq pdf-annot-activate-created-annotations t)
+;; more fine-grained zooming
+(setq pdf-view-resize-factor 1.1)
+;; set some default properties for all annotations
+(setq pdf-annot-default-annotation-properties
+      '((t
+         (label . "carlos")
+         (color . "#483d8b"))
+        (text
+         (icon . "Note"))
+        (highlight
+         (color . "#eee8aa"))
+        (squiggly
+         (color . "orange"))
+        (strike-out
+         (color . "red"))))
 
 ;; Magit config
 (add-hook! magit-mode (visual-line-mode +1))
