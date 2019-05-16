d73674cf7f79459e03dfac67e6cdaf21c5ef1d4d
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2019-05-16 11:08:52 +0200

    company: do not show company automatically

--- +completion.el
+++ +completion.el
@@ -1,5 +1,8 @@
 ;;; ~/.doom.d/+completion.el -*- lexical-binding: t; -*-
 
+(after! company
+  (setq company-idle-delay nil))
+
 (after! ivy
   ;; Add a kill action to Ivy's buffer switching
   (ivy-set-actions 'ivy-switch-buffer '(("k" kill-buffer "kill")))
