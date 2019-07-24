ff79e52afd076803659b6a848e439b9c3d76682f
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2019-07-24 11:28:32 +0200

    init: enable ivy posframe, my center-window mod

--- init.el
+++ init.el
@@ -36,13 +36,15 @@
        (company          ; the ultimate code completion backend
         +childframe)
        (ivy              ; a search engine for love and life
-        ;; +childframe
+        +childframe
+        +prescient
         +icons)
        ;;helm            ; the *other* search engine for love and life
        ;;ido             ; the other *other* search engine...
 
        :ui
        ;;deft            ; notational velocity for Emacs
+       center-window
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
