893481381cf0aeb0fad7ca0d607e7199b108dd40
Undead Kernel <carloscg@gmail.com>
2020-05-11 12:06:33 +0200

    undo: don't save history

--- config.el
+++ config.el
@@ -62,7 +62,8 @@
 ;; Scrolling commands do not cancel isearch
 (setq isearch-allow-scroll t)
 
-;; Don't save undo-tree history
+;; Don't save undo history
+(remove-hook 'undo-fu-mode-hook #'global-undo-fu-session-mode)
 (after! undo-tree
   (setq undo-tree-auto-save-history nil))
 
