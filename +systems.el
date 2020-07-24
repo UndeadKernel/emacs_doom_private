376bd7983a3aaf0d5af78cb260d4ac49e06a1c4b
U-VCS\cordero <cordero@SPC-VM-C-CG.vcs.de>
2020-07-24 12:29:21 +0200

    system: add another custom system config

--- +systems.el
+++ +systems.el
@@ -17,3 +17,24 @@
           org-archive-location (concat org-directory ".archive/%s::")
           +bibliography-notes-dir "~/Documents/work/org/bib/")
     (add-to-list 'org-file-apps '("\\.docx?\\'" . system))))
+
+(when (string= "SPC-VM-C-CG" (system-name))
+  (after! ivy
+    ;; remove the path separator that would make rg from chocolatey
+    ;; ... exit with an error
+    (setq counsel-rg-base-command
+          (seq-difference counsel-rg-base-command
+                          '(" --path-separator=//"
+                            " --path-separator //"
+                            "--path-separator"
+                            "//"))))
+  ;; org directory of work env
+  (after! org
+    (setq org-directory "~/Documents/work/org/"
+          org-agenda-files (list org-directory)
+          org-archive-location (concat org-directory ".archive/%s::")
+          +bibliography-notes-dir "~/Documents/work/org/bib/")
+    (add-to-list 'org-file-apps '("\\.docx?\\'" . system)))
+
+  ;; use putty's plink as default tramp method
+  (setq tramp-default-method "plink"))
