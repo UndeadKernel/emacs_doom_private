eed77acd9a4a9aed9c548cfdc67bac5daff4ca05
UndeadKernel <carloscg@gmail.com>
2020-11-27 10:48:01 +0100

    init: make org mode pretty

--- init.el
+++ init.el
@@ -173,7 +173,8 @@
         +jupyter           ; ipython/jupyter support for babel
         +pandoc            ; export-with-pandoc support
         ;;+pomodoro        ; be fruitful with the tomato technique
-        +present)          ; using org-mode for presentations
+        +present           ; using org-mode for presentations
+        +pretty)
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
