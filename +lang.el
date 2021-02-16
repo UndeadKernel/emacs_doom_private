b07535194abf66b9ca6d1528894d4f7b8c297745
UndeadKernel <carloscg@gmail.com>
2021-02-16 21:39:49 +0100

    lang: better WEB/JS defaults, restclient bugfix
    Enable restclient commands to start with spaces.

--- +lang.el
+++ +lang.el
@@ -3,3 +3,17 @@
 ;; lsp-ui-sideline is redundant with eldoc and much more invasive
 (setq lsp-ui-sideline-enable nil
       lsp-enable-symbol-highlighting nil)
+
+;; web mode indentation level
+(setq web-mode-markup-indent-offset 2
+      web-mode-sql-indent-offset 2
+      web-mode-css-indent-offset 2
+      web-mode-code-indent-offset 2)
+
+;; typescript indentation level
+(setq-default typescript-indent-level 2)
+
+(after! restclient
+  ;; allow restclient queries to start with spaces (so as to be able to indent them)
+  (setq restclient-method-url-regexp
+   "^[[:blank:]]*\\(GET\\|POST\\|DELETE\\|PUT\\|HEAD\\|OPTIONS\\|PATCH\\) \\(.*\\)$"))
