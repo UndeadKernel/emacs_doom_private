dfb0a2eb300f87de315ec774ef19e18c58a03e53
UndeadKernel <carloscg@gmail.com>
2022-02-08 19:17:05 +0100

    mouse: better mouse scrolling

--- config.el
+++ config.el
@@ -36,9 +36,10 @@
 (load! "+term")
 
 ;; Smooth mouse scrolling
-(setq mouse-wheel-scroll-amount '(1) ; scroll two lines at a time
-      mouse-wheel-progressive-speed t ; don't accelerate scrolling
-      mouse-wheel-follow-mouse t) ; scroll window under mouse
+(setq mouse-wheel-scroll-amount '(2 ((shift) . 1))  ; scroll two lines at a time
+      mouse-wheel-progressive-speed nil             ; don't accelerate scrolling
+      mouse-wheel-follow-mouse t                    ; scroll window under mouse
+      scroll-step 1)
 
 ;; Backups
 (setq make-backup-files t
