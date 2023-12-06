3f4b63dbc2cb5d5af338df884ce9bed0381a7d6a
UndeadKernel <6165006+UndeadKernel@users.noreply.github.com>
2023-12-06 12:02:43 +0100

    jupyter: pin to stable version (new doesn't work)

--- packages.el
+++ packages.el
@@ -9,3 +9,7 @@
 (package! pacfiles-mode)
 
 (package! command-log-mode)
+
+;; Current version is broken with Org 9.7
+;;(package! jupyter :pin "16cbda79167b4e2f2c6b61b218658f0f660d97f9")
+(package! jupyter :pin "053a78da252b19cf59cefe6b83f9c4531a38d6b2")
