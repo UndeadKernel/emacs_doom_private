0e05c49d136de06099debf6be4b12f2c1988ab98
UndeadKernel <6165006+UndeadKernel@users.noreply.github.com>
2025-02-28 13:42:02 +0100

    tramp: disable backups when editing through Tramp

--- +editor.el
+++ +editor.el
@@ -1,5 +1,16 @@
 ;;; ~/emacs/doom_private/+editor.el -*- lexical-binding: t; -*-
 
+(after! tramp
+  ;; Disable backups when editing files through Tramp.
+  (setq +boy/disable-tramp-backups '("ssh" "sftp")) ;; disable 'ssh' and 'sftp'
+  (setq backup-enable-predicate
+        (lambda (fname)
+          (and (normal-backup-enable-predicate fname)
+               ;; Disable tramp backups for the given methods
+               (not (and +boy/disable-tramp-backups
+                         (or
+                          (member "all" +boy/disable-tramp-backups)
+                          (member (file-remote-p fname 'method) +boy/disable-tramp-backups))))))))
 
 (after! objed
   ;; Never start objed automatically
