844344925acd848cb1171abb95992c39a26f16de
UndeadKernel <6165006+UndeadKernel@users.noreply.github.com>
2025-02-28 13:43:00 +0100

    lang: set a nice C++ indentation style

--- +lang.el
+++ +lang.el
@@ -21,3 +21,15 @@
   ;; allow restclient queries to start with spaces (so as to be able to indent them)
   (setq restclient-method-url-regexp
    "^[[:blank:]]*\\(GET\\|POST\\|DELETE\\|PUT\\|HEAD\\|OPTIONS\\|PATCH\\) \\(.*\\)$"))
+
+(after! cc-mode
+  (c-add-style "c-boy-style" '((c-tab-always-indent . t)
+                               (c-basic-offset . 2)
+                               (c-indent-level . 2)
+                               (c-offsets-alist (access-label . --)
+                                                (label . +)
+                                                (innamespace . [0]))))
+
+  (when (listp c-default-style)
+    (setf (alist-get 'c-mode c-default-style) "c-boy-style")
+    (setf (alist-get 'c++-mode c-default-style) "c-boy-style")))
