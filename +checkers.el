44d1ab8982b85504dc2064229ec9657864a9ccde
UndeadKernel <6165006+UndeadKernel@users.noreply.github.com>
2026-04-08 16:17:24 +0200

    flycheck: change popup position, load path
    Consolidate checkers into one file.

--- +checkers.el
+++ +checkers.el
@@ -1,5 +1,8 @@
 ;;; +checkers.el -*- lexical-binding: t; -*-
 
 (after! flycheck
-  (setq flycheck-posframe-position 'frame-top-right-corner
-        flycheck-posframe-border-width 4))
+  ;; Change the default placement of the flycheck "popup"
+  (setopt flycheck-posframe-position 'point-bottom-left-corner
+          flycheck-posframe-border-width 4)
+  ;; Let flycheck search for required files in the `load-path' and the current folder.
+  (setopt flycheck-emacs-lisp-load-path '("./")))
--- +feature.el
+++
@@ -1,4 +0,0 @@
-;;; +feature.el -*- lexical-binding: t; -*-
-
-;; Let flycheck search for required files in the `load-path' and the current folder.
-(setq flycheck-emacs-lisp-load-path '("./"))
