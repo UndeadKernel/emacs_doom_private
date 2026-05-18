03f969e67e426d374611879ffbbfcb2d82a57614
Carlos Garcia Cordero <carlos.garciacordero@esa.int>
2026-05-18 16:28:24 +0200

    checkers: disable org checks, default load path

--- +checkers.el
+++ +checkers.el
@@ -5,4 +5,6 @@
   (setopt flycheck-posframe-position 'point-bottom-left-corner
           flycheck-posframe-border-width 4)
   ;; Let flycheck search for required files in the `load-path' and the current folder.
-  (setopt flycheck-emacs-lisp-load-path '("./")))
+  ;; (setopt flycheck-emacs-lisp-load-path '("./"))
+  ;; Temp: disable org-lint because there's a bug that causes a syntax error
+  (setopt flycheck-disabled-checkers '(org-lint)))
